c690b8163ec02b06b6dd701a81a91655c267cc1d
cc-java-d <ccjavad@gmail.com>
2019-09-13 09:33:32 +0200

    update

---
+++ Projekte/Imperativ/src/variablen/PrimitiveDatentypen.java
@@ -0,0 +1,47 @@
+package variablen;
+
+public class PrimitiveDatentypen {
+	
+	public static void main(String[]args) {
+
+		/*
+		 * 
+		 * Numberische
+		 * 
+		 */
+		
+		/*
+			ganzahlige
+		 */
+		byte b;		// 8 bit		[-128 .. 127]
+		
+		short s;	// 16 bit
+		
+		int x; 		// 32 bit 
+		
+		long var;	// 64 bit
+		
+		
+		/*
+		 	Gleitkomma-Typen
+		 */
+		float f;	// 32 bit
+		
+		double d;	// 64 bit
+		
+		
+		/*
+		 	Zeichen-Typ
+		 */
+		char ch;	// 16 (nichtnegativ)
+		
+		
+		/*
+		 	Wahrheits-Typ
+		 */
+		boolean bool; 	// ?
+		
+	}
+	
+	
+}
