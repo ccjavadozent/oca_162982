9c02171961779dcc77170b8f92b695fe3d07d7dc
cc-java-d <ccjavad@gmail.com>
2019-10-21 12:33:58 +0200

    update

---
+++ Projekte/JavaAPI/src/aufgabe/KleineAufgabeFormat.java
@@ -0,0 +1,25 @@
+package aufgabe;
+
+public class KleineAufgabeFormat {
+
+	public static void main(String[] args) {
+
+		/*
+		 * A1
+		 * 
+		 * 
+		 * Bitte das Array mit Zufallswerten als eine Tabelle ausgeben:
+		 * 
+		 * |     Nr.| 		Wert  |
+		 * |	 01 | 	   121314 |
+		 * |     02 | 		  -22 |
+		 * ...
+		 * 
+		 */
+		int[] array = randomArray(); // Array Länge 10 mit Zufallswerten aus dem Bereich [-10000 ... 10000] 
+		print(array);
+		
+		
+	}
+
+}
