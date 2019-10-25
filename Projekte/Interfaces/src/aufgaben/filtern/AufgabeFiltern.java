54859fc7f5adaa00a5ffe2c596e5bd36612843cc
cc-java-d <ccjavad@gmail.com>
2019-10-25 12:43:54 +0200

    update

--- Projekte/Interfaces/src/aufgaben/filtern/AufgabeFiltern.java
+++ Projekte/Interfaces/src/aufgaben/filtern/AufgabeFiltern.java
@@ -30,10 +30,35 @@ public class AufgabeFiltern {
 		 * Die neue ArrayList wird zurück geliefert.
 		 * 
 		 */
-		ArrayList<Integer> listGerade = filtern(list);
+		ArrayList<Integer> listGerade = filtern(list, logikZumAuswählenDerGeradenZahlen);
 		System.out.println("list gerade: " + listGerade); // 12, 14, -4
+		
+		/*
+		 * Ändern Sie die Methode 'filtern' so, dass sie nicht selbst den Auswahlskriterium definiert, 
+		 * sondern dem Benutzer es überlässt:
+		 *  
+		 */
+		
+		ArrayList<Integer> listPositive = filtern(list, logikZumAuswählenDerPositivenZahlen);
+		System.out.println("list positive: " + listPositive); // 12, 13, 14, 15, 77
+
+		
+		ArrayList<Integer> listStartenMit1 = filtern(list, logikZumAuswählenDerZahlenDieMit1Starten);
+		System.out.println("list positive: " + listPositive); // 12, 13, 14, 15
 		
 		
 	} // end of main
 
+	static ArrayList<Integer> filtern(ArrayList<Integer> list, param) {
+		ArrayList<Integer> back = new ArrayList<>();
+		
+		for (Integer value : list) {
+			if(value %2 == 0) {
+				back.add(value);
+			}
+		}
+		
+		return back;
+	}
+	
 }
