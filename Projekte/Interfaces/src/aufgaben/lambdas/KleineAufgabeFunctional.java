fa7e311ec57c0c6a0547ab5159c7677412192db5
cc-java-d <ccjavad@gmail.com>
2019-10-30 12:09:55 +0100

    update

---
+++ Projekte/Interfaces/src/aufgaben/lambdas/KleineAufgabeFunctional.java
@@ -0,0 +1,20 @@
+package aufgaben.lambdas;
+
+public class KleineAufgabeFunctional {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * A1. 
+		 * 
+		 * Bitte ein Array 'namen' mit folgenden Strings erstellen:
+		 * 	
+		 * "Tom", "Jerry", "Peter", "Paul", "Mary", "Ute"
+		 * 
+		 */
+
+		
+		
+	}
+
+}
---
+++ Projekte/Interfaces/src/functional/B10_Predicate_und_ArrayList.java
@@ -0,0 +1,78 @@
+package functional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class B10_Predicate_und_ArrayList {
+	
+	static class IsNegative implements Predicate<Integer> {
+		public boolean test(Integer x) {
+			return x < 0;
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<>();
+		list.add(12);
+		list.add(-3);
+		list.add(55);
+		list.add(-17);
+		list.add(100);
+
+		System.out.println("list: " + list);
+		
+		List<Integer> listCopy = new ArrayList<Integer>( list );
+		System.out.println("1. listCopy: " + listCopy);
+		
+		Predicate<Integer> filter = new IsNegative();
+		
+		/*
+		 * removeIf(Predicate)
+		 * 	entfernt alle Elemente aus der List, für die die test-Methode des Parameters (Predicate) true liefert
+		 * 
+		 */
+		listCopy.removeIf(filter);
+		System.out.println("Negative entfernt: " + listCopy ); // [12, 55, 100]
+		
+		
+		listCopy = new ArrayList<Integer>( list );
+		System.out.println("2. listCopy: " + listCopy);
+		
+		listCopy.removeIf( x -> x > 0 );
+
+		System.out.println("Positive entfernt: " + listCopy ); // [-3, -17]
+		
+		System.out.println("---------------------");
+		/*
+		 * Predicates ableiten/kombinieren
+		 */
+		
+		Predicate<Integer> isGerade = x -> x % 2 == 0;
+		Predicate<Integer> isUngeradeA = x -> !isGerade.test(x); 
+		Predicate<Integer> isUngeradeB = isGerade.negate(); 
+		
+		System.out.println("ist 12 gerade: " + isGerade.test(12));
+		System.out.println("ist 12 ungerade: " + isUngeradeA.test(12));
+		System.out.println("ist 12 ungerade: " + isUngeradeB.test(12));
+		System.out.println("ist 12 ungerade: " + isGerade.negate().test(12));
+		
+		Predicate<Integer> isZero = x -> x==0;
+		
+		Predicate<Integer> isUngeradeOderZero = isUngeradeA.or(isZero);
+		
+		System.out.println( "ist 33 ungerade oder 0: " + isUngeradeOderZero.test(33) );	// true
+		System.out.println( "ist 0 ungerade oder 0: " + isUngeradeOderZero.test(0) );	// true
+		System.out.println( "ist -12 ungerade oder 0: " + isUngeradeOderZero.test(-12) ); // false
+		
+		listCopy = new ArrayList<Integer>(list);
+		listCopy.add(2, 0);
+		System.out.println("neue Liste: " + listCopy); // [12, -3, 0, 55, -17, 100]
+		
+		listCopy.removeIf( isUngeradeOderZero );
+		System.out.println("neue Liste ohne 0 und ohne ungeraden: " + listCopy); // [12, 100]
+		
+	} 
+
+}
