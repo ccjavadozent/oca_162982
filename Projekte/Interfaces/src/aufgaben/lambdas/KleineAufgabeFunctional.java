9ef51983aa211d818d448cb010050a273b18f822
cc-java-d <ccjavad@gmail.com>
2019-10-30 12:24:15 +0100

    update

--- Projekte/Interfaces/src/aufgaben/lambdas/KleineAufgabeFunctional.java
+++ Projekte/Interfaces/src/aufgaben/lambdas/KleineAufgabeFunctional.java
@@ -1,5 +1,20 @@
 package aufgaben.lambdas;
 
+import java.util.List;
+
+class Astronaut {
+	private String name;
+
+	public Astronaut(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Austronaut " + name;
+	}
+}
+
 public class KleineAufgabeFunctional {
 
 	public static void main(String[] args) {
@@ -13,8 +28,29 @@ public class KleineAufgabeFunctional {
 		 * 
 		 */
 
+		String[] namen = { "Tom", "Jerry", "Peter", "Paul", "Mary", "Ute" };
+		
+		/*
+		 * A2.
+		 * 
+		 * Klasse 'Astronaut' mit 
+		 * - einem String-Attribut 'name'
+		 * - Konstruktor (das Attribut initialisieren)
+		 * - toString: return "Austronaut " + name;
+		 * 
+		 */
+		
+		/*
+		 * A3.
+		 * 
+		 * Bitte die Methode 'buildMannschaft' realisieren, die die List mit Astronauten erzeugt,
+		 * und namen für die Astronauten aus dem übergebenen Array nimmt.
+		 * Der 2. Parameter beschreibt die Logik zum Bilden eines Astronauten. 
+		 */
+		List<Astronaut> listA = buildMannschaft(namen, n -> new Astronaut(n));
 		
 		
-	}
+		
+	} // end of main
 
 }
